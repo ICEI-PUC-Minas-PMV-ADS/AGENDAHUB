07afa3e87de569ac9762e034cd0f186422a4edd4
Alvaro <alvarogoncalves923@gmail.com>
2023-10-25 20:25:04 -0300

    Add layout LoginCadastro

---
+++ src/AGENDAHUB/AGENDAHUB/wwwroot/js/site - Cópia.js
@@ -0,0 +1,11 @@
+﻿const openMenuBtn = document.getElementById("openMenu");
+const closeMenuBtn = document.getElementById("closeMenu");
+const myOffcanvas = new bootstrap.Offcanvas(document.getElementById('myOffcanvas'));
+
+openMenuBtn.addEventListener("click", () => {
+    myOffcanvas.show();
+});
+
+closeMenuBtn.addEventListener("click", () => {
+    myOffcanvas.hide();
+});
