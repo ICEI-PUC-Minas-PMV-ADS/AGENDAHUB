854a6a0e817fcbaa5d22bb2972e14a9ba58ef2d5
Alvaro <alvarogoncalves923@gmail.com>
2023-11-14 22:55:29 -0300

    Add MostrarSenhaLogin

---
+++ src/AGENDAHUB/AGENDAHUB/wwwroot/js/MostrarSenha.js
@@ -0,0 +1,14 @@
+﻿// MostrarSenha.js
+function MostrarSenha() {
+    var senhaInput = document.getElementById('textSenha');
+    var iconeSenha = document.getElementById('iconeSenha');
+
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+        iconeSenha.src = '/images/Eye.png';
+    } else {
+        senhaInput.type = 'password';
+        iconeSenha.src = '/images/hideEye.png';
+    }
+}
+
